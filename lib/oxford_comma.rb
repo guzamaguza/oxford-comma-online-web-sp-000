bc27b76c2700cc10642512c8e865f1db913f4bb8
Mark A Guza <guzamaguza@gmail.com>
2020-01-13 05:53:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   finalString = ""
 
-  i<array.length;i++ do {
+
   array.each do |word|
     if i < array.length - 1
      finalString = finalString + word + ","
@@ -10,6 +10,6 @@ def oxford_comma(array)
     elsif i == array.length
      finalString = finalString + word
     end
-   }
+  end
   return finalString
 end
