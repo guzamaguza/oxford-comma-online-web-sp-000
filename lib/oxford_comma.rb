82425e29e60132f7c19d63df7480c96c50608d74
Mark A Guza <guzamaguza@gmail.com>
2020-01-13 05:47:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   finalString = ""
 
-  for let i=0;i<array.length;i++
+  for let i=0;i<array.length;i++ do
     if i < array.length - 1
      finalString = finalString + array[i] + ","
     elsif i == array.length-1
