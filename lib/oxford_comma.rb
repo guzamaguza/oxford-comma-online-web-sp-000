5bc1931cac0fb4b54b6c1cb6af9540f1a9b5d2ab
Mark A Guza <guzamaguza@gmail.com>
2020-01-13 05:59:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,16 +5,16 @@ def oxford_comma(array)
   array.each do |word|
     if array.length == 1
       return word
-    end
+    else
 
-    if i < array.length - 1
-     finalString = finalString + word + ","
-    elsif i == array.length-1
-     finalString = finalString + word + ", and"
-    elsif i == array.length
-     finalString = finalString + word
+        if i < array.length - 1
+         finalString = finalString + word + ","
+        elsif i == array.length-1
+         finalString = finalString + word + ", and"
+        elsif i == array.length
+         finalString = finalString + word
+        end
     end
-
     i += 1
   end
   return finalString
