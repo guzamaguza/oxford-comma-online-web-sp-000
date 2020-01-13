e2bb1fd3d378a7c2a084e3bd64f18232006d3f1d
Mark A Guza <guzamaguza@gmail.com>
2020-01-13 06:03:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,13 +2,12 @@ def oxford_comma(array)
   finalString = ""
   i = 0
 
-
+array.each do |word|
   if array.length == 1
     return word
   elsif array.length == 2
     return array[0] + "and" + array[1]
   else
-  array.each do |word|
         if i < array.length - 1
          finalString = finalString + word + ","
         elsif i == array.length-1
