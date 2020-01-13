dc17e06ed5e669b5f7ddf5ba061679b341d55a61
Mark A Guza <guzamaguza@gmail.com>
2020-01-13 05:46:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  finalString = ""
 
+  for (let i=0;i<array.length;i++)
+    if i < array.length - 1
+     finalString = finalString + array[i] + ","
+    elsif i == array.length-1
+     finalString = finalString + array[i] + ", and"
+    elsif i == array.length
+     finalString = finalString + array[i]
+    end
+   end
+  return finalString
 end
